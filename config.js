c0471e641eb3c24fb9168502bba9011b10c0a75a
Road Rider's <87977520+RoadRiders@users.noreply.github.com>
2023-01-08 19:41:36 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: 'MTA0ODcyNzA1NzI1OTA1MzIwNg.GQfY08.ex9LS2LPjTchFcdwj1Mv4jQBBSKDsOqo1dosZU',
+        owners: '517314324209991698', //id buyer
+        funny: '1048727057259053206', //id Développeur
+        color: '#ff0000', //couleur par défaut
+        footer: 'Red Line Transport +help',
+        maxVol: '25',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
