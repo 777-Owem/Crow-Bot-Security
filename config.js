859fd73b0bc5e450a3a598ea426dccf0b15edead
Road Rider's <87977520+RoadRiders@users.noreply.github.com>
2023-01-08 19:42:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     app: {
         px: '+',
-        token: 'MTA0ODcyNzA1NzI1OTA1MzIwNg.GQfY08.ex9LS2LPjTchFcdwj1Mv4jQBBSKDsOqo1dosZU',
-        owners: '517314324209991698', //id buyer
-        funny: '1048727057259053206', //id Développeur
+        token: 'TOKEN DU BOT',
+        owners: 'TON ID', //id buyer
+        funny: 'ID DE TON BOT', //id Développeur
         color: '#ff0000', //couleur par défaut
-        footer: 'Red Line Transport +help',
+        footer: 'ton footer de ton choix +help',
         maxVol: '25',
         everyoneMention: false,
         hostedBy: true,
